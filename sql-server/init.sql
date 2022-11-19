e0460ae367f9be62bd1f7c05bb25e4399ec664a4
Dominik <dominik.mehringer@live.de>
2022-11-19 23:49:22 +0100

    Add user table

--- sql-server/init.sql
+++ sql-server/init.sql
@@ -1,6 +1,7 @@
 DROP DATABASE test_db;
 CREATE DATABASE IF NOT EXISTS test_db;
 USE test_db;
+CREATE TABLE IF NOT EXISTS `test_db`.`User` ( `UserID` INT NOT NULL AUTO_INCREMENT , `Email` TINYTEXT NOT NULL , `Role` TINYTEXT NOT NULL , `Name` TINYTEXT NOT NULL , `Password` TINYTEXT NOT NULL , PRIMARY KEY (`UserID`) ) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `test_db`.`Bookkeeping` ( `OrderID` INT NOT NULL AUTO_INCREMENT , `Security` TINYTEXT NOT NULL , `Qty` INT NOT NULL , `Price` DECIMAL NOT NULL , `Operation` TINYTEXT NOT NULL , `Side` TINYTEXT NOT NULL , `UserID` INT NOT NULL , PRIMARY KEY (`OrderID`)) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `test_db`.`Exchange` ( `ExchangeID` INT NOT NULL AUTO_INCREMENT , `Security` TINYTEXT NOT NULL , `UserID` INT NOT NULL , `Price` DECIMAL NOT NULL , `Qty` INT NOT NULL , `Side` TINYTEXT NOT NULL , PRIMARY KEY (`ExchangeID`)) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `test_db`.`Matches` ( `MatchID` INT NOT NULL AUTO_INCREMENT , `Security` TINYTEXT NOT NULL , `Seller` INT NOT NULL , `SellerOrderID` INT NOT NULL , `Buyer` INT NOT NULL , `BuyerOrderID` INT NOT NULL , `Qty` INT NOT NULL , `Price` DECIMAL NOT NULL , PRIMARY KEY (`MatchID`) ) ENGINE = InnoDB;
@@ -9,3 +10,4 @@ INSERT INTO `Matches` (`MatchID`, `Security`, `Seller`, `SellerOrderID`, `Buyer`
 INSERT INTO `Bookkeeping` (`OrderID`, `Security`, `Qty`, `Price`, `Operation`, `Side`, `UserID`) VALUES (NULL, 'Twitter', '5', '110', 'add', 'buy', '1'); 
 INSERT INTO `Bookkeeping` (`OrderID`, `Security`, `Qty`, `Price`, `Operation`, `Side`, `UserID`) VALUES (NULL, 'Twitter', '10', '90', 'add', 'sell', '1');
 INSERT INTO `Bookkeeping` (`OrderID`, `Security`, `Qty`, `Price`, `Operation`, `Side`, `UserID`) VALUES (NULL, 'Twitter', '5', '110', 'del', 'sell', '1');  
+INSERT INTO `User` (`UserID`, `Email`, `Name`, `Password`, `Role`) VALUES (0, 'root@secex.com', 'Root', 'd0508cb8ac4296ebff9aafb902978f0ef167b6c03aa332bef035b521aabe6a18', 'admin');  
